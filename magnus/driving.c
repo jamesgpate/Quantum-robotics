761013955c235d10df6c8ea977db54828cefdc66
notjoms <jgp5232002@gmail.com>
2016-12-19 12:52:27 -0600

    framework for new robot

---
+++ magnus/driving.c
@@ -0,0 +1,10 @@
+#include "driving.c"
+task driving()
+{
+	while(true){
+		motor[left13]=vexRT[Ch3];
+		motor[left24]=-vexRT[Ch3];
+		motor[right13]=vexRT[Ch2];
+		motor[right24]=-vexRT[Ch2];
+	}
+}
