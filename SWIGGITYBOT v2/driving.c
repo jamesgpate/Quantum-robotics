8672365032ac8fa8da3bb9acff1b9152363dbd91
notjoms <jgp5232002@gmail.com>
2017-01-26 15:51:17 -0600

    weeeew

--- SWIGGITYBOT v2/driving.c
+++ SWIGGITYBOT v2/driving.c
@@ -1,6 +1,9 @@
 #include "mainfile.c"
 task driving(){
 	int c1 = 0, c3 = 0, c4 = 0, threshold = 15;
+	SensorValue[LBEnc] = 0;
+	SensorValue[RBEnc] = 0;
+	SensorValue[ArmEnc] = 0;
 	while(true){
 		c1 = vexRT[Ch1];
 		c3 = vexRT[Ch3];
@@ -20,7 +23,19 @@ task driving(){
 			motor[L1R1]=-127;
 			motor[L2R2]=-127;
 			motor[L3R3]=-127;
-		}else if(vexRT[Btn6D]==0&&vexRT[Btn5D]==0){
+		}else if(vexRT[Btn8D]==1){
+			motor[L1R1]=30;
+			motor[L2R2]=30;
+			motor[L3R3]=30;
+		}else if(vexRT[Btn8R]==1){
+			motor[L1R1]=60;
+			motor[L2R2]=60;
+			motor[L3R3]=60;
+		}else if(vexRT[Btn8U]==1){
+			motor[L1R1]=90;
+			motor[L2R2]=90;
+			motor[L3R3]=90;
+		}else if(vexRT[Btn6D]==0&&vexRT[Btn5D]==0&&vexRT[Btn8D]==0&&vexRT[Btn8R]==0&&vexRT[Btn8U]==0){
 			motor[L1R1]=0;
 			motor[L2R2]=0;
 			motor[L3R3]=0;
@@ -31,6 +46,7 @@ task driving(){
 		if(vexRT[Btn6U]==0){
 			SensorValue[dgtl1]=0;
 		}
+		if(SensorValue[ArmEnc]>=220) motor[L1R1]=motor[L2R2]=motor[L2R2]=0;
 		clearLCDLine(0);
 		clearLCDLine(1);
 		displayNextLCDNumber(SensorValue[pot1]);
